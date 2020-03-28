fae4a006bb803ef2381451ad10d0011357377aa7
Vincent Rüegge <vincent@rueegge.ch>
2020-03-28 13:28:31 +0100

    argument passing between pages and minor structure/styling

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ app = Flask(__name__)
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        brandname = request.form["brandname"]
-        return render_template("index.html", brandname=brandname)
+        brandname = request.form["nm"]
+        return redirect(url_for("predict", bn=brandname))
     else:
         return render_template("index.html")
+
+
+@app.route("/<bn>", methods=["POST", "GET"])
+def predict(bn):
+    return render_template("predict.html", brandname=bn)
 
 
 if __name__ == "__main__":
