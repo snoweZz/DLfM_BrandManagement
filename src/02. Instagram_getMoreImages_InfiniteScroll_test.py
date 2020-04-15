c140eaf58be4d5e484e68ad2cbad6979b6b3dd9b
linda <linda.samsinger@uzh.ch>
2020-04-15 16:49:14 +0200

    hashtag working too

--- src/02. Instagram_getMoreImages_InfiniteScroll_test.py
+++ src/02. Instagram_getMoreImages_InfiniteScroll_test.py
@@ -10,11 +10,14 @@ import os
 from random import choice
 import json
 import pandas as pd
+import numpy as np
 import csv
 # load modules 
-import imageio
-import json
-import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+from keras.preprocessing.image import load_img, img_to_array
+import shutil 
 # to install
 import requests
 from bs4 import BeautifulSoup
@@ -22,9 +25,7 @@ from bs4 import BeautifulSoup
 # TODO: for inofficial instagram page 
 # TODO: without saving on PC, display image in IDE 
 
-# set directory 
-directory= r"C:\Users\lsamsi\Documents\GitHub\DLfM_BrandManagement\data\instagram_images\nestle"
-os.chdir(directory)
+
 
 
 #%%
@@ -34,20 +35,68 @@ from instagram_private_api import Client, ClientCompatPatch
 
 user_name = 'chenpeling@hotmail.com'
 password = 'Instagram2020'
-username_to_scrape = 'nestle' # official nestle page: 'nestle'
-url = "https://www.instagram.com/nestle/"
+USERNAME = 'nestle' # official nestle page: 'nestle'
+HASHTAG = 'longines'
+LIMIT_IMAGE_COUNT = 300 
 
 
 # initialize client 
 api = Client(user_name, password)
 
+
+
+#%%
+########### HASHTAG ###################
+
 # all images urls 
-all_image_posts_urls = []
+all_hash_image_posts_urls = []
+flat_hash_image_posts_urls = [item for sublist in all_hash_image_posts_urls for item in sublist]
 next_max_id = None
-while api.username_feed(username_to_scrape, max_id = next_max_id)["more_available"] == True: 
+while (api.feed_tag(HASHTAG, api.generate_uuid())["more_available"] == True) and (len(flat_hash_image_posts_urls) <= LIMIT_IMAGE_COUNT): 
     if next_max_id == None: 
         #Gets the first 12 posts
-        posts = api.username_feed(username_to_scrape)
+        posts = api.feed_tag(HASHTAG, api.generate_uuid())
+        len(posts['items'])
+        image_urls = []
+        for i in range(len(posts['items'])): 
+            try: 
+                url = posts['items'][i]['image_versions2']['candidates'][0]['url'] # some posts do not have 'image_version2', they are overlooked in that case
+                image_urls.append(url)
+            except: 
+                pass 
+        # Extract the value *next_max_id* from the above response, this is needed to load the next 12 posts
+        next_max_id = posts["next_max_id"] 
+        all_hash_image_posts_urls.append(image_urls)
+    else: 
+        next_page_posts = api.feed_tag(HASHTAG, api.generate_uuid())
+        len(next_page_posts['items'])
+        # get image urls 
+        next_image_urls = []
+        for i in range(len(next_page_posts['items'])):
+            try: 
+                url = next_page_posts['items'][i]['image_versions2']['candidates'][0]['url']
+                next_image_urls.append(url)
+            except: 
+                pass
+        # Extract the value *next_max_id*
+        next_max_id = next_page_posts["next_max_id"] 
+        all_hash_image_posts_urls.append(next_image_urls)
+        
+
+
+print(f"A total of {len(flat_hash_image_posts_urls)} image post urls were retrieved from the Instagram page.")
+
+#%%
+########### USERNAME ###################
+
+# all images urls 
+all_image_posts_urls = []
+flat_image_posts_urls = [item for sublist in all_image_posts_urls for item in sublist]
+next_max_id = None
+while (api.username_feed(USERNAME, max_id = next_max_id)["more_available"] == True)  and (len(flat_image_posts_urls) <= LIMIT_IMAGE_COUNT): 
+    if next_max_id == None: 
+        #Gets the first 12 posts
+        posts = api.username_feed(USERNAME)
         len(posts['items'])
         image_urls = []
         for i in range(len(posts['items'])): 
@@ -57,7 +106,7 @@ while api.username_feed(username_to_scrape, max_id = next_max_id)["more_availabl
         next_max_id = posts["next_max_id"] 
         all_image_posts_urls.append(image_urls)
     else: 
-        next_page_posts = api.username_feed(username_to_scrape, max_id = next_max_id)
+        next_page_posts = api.username_feed(USERNAME, max_id = next_max_id)
         len(next_page_posts['items'])
         # get image urls 
         next_image_urls = []
@@ -72,8 +121,12 @@ while api.username_feed(username_to_scrape, max_id = next_max_id)["more_availabl
         all_image_posts_urls.append(next_image_urls)
         
 
-flat_image_posts_urls = [item for sublist in all_image_posts_urls for item in sublist]
+
 print(f"A total of {len(flat_image_posts_urls)} image post urls were retrieved from the Instagram page.")
+
+
+
+### Request Errors: 
 # ClientConnectionError: URLError <urlopen error timed out>
         
 # if Timeout: spyder/plugins/ipythonconsole/comms/kernelcomm.py 
@@ -96,20 +149,32 @@ api = Client(user_name, password, cookie = cookies)
 
 
 
+
 #%%
-import matplotlib.pyplot as plt
-from PIL import Image
-from io import BytesIO
-from keras.preprocessing.image import load_img, img_to_array
-import numpy as np
-import shutil 
 
+# set directory 
+directory= r"C:\Users\lsamsi\Documents\GitHub\DLfM_BrandManagement\data\instagram_images\football"
+os.chdir(directory)
 
-# save images to PC 
-for i in range(len(all_image_posts_urls)):
-    for j in range(len(all_image_posts_urls[i])):
-        r = requests.get(all_image_posts_urls[i][j], stream=True)
-        with open(f"{i}_"+all_image_posts_urls[i][j][-34:]+".png", 'wb') as f:
+          
+# save HASHTAG images to PC 
+for i in range(len(flat_hash_image_posts_urls)):
+    r = requests.get(flat_hash_image_posts_urls[i], stream=True)
+    with open(f"{i}_"+flat_hash_image_posts_urls[i][-34:]+".png", 'wb') as f:
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        r.raw.decode_content = True
+        # Copy the response stream raw data to local image file.
+        shutil.copyfileobj(r.raw, f)
+        # Remove the image url response object.
+        del r
+        
+ #%%       
+
+# save USERNAME images to PC 
+for i in range(len(flat_image_posts_urls)):
+    for j in range(len(flat_image_posts_urls[i])):
+        r = requests.get(flat_image_posts_urls[i][j], stream=True)
+        with open(f"{i}_"+flat_image_posts_urls[i][j][-34:]+".png", 'wb') as f:
             # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
             r.raw.decode_content = True
             # Copy the response stream raw data to local image file.
