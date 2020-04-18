368f70fd706bc9a2c6962c3969bb392d2344b567
linda <linda.samsinger@uzh.ch>
2020-04-18 16:17:59 +0200

    add Infinite Scroll to .ipynb jupyter

--- src/02. Instagram_getMoreImages_InfiniteScroll_test.py
+++ src/02. Instagram_getMoreImages_InfiniteScroll_test.py
@@ -22,7 +22,7 @@ import shutil
 import requests
 from bs4 import BeautifulSoup
 
-# TODO: for inofficial instagram page 
+
 # TODO: without saving on PC, display image in IDE 
 
 
@@ -37,7 +37,7 @@ user_name = 'chenpeling@hotmail.com'
 password = 'Instagram2020'
 USERNAME = 'nestle' # official nestle page: 'nestle'
 HASHTAG = 'longines'
-LIMIT_IMAGE_COUNT = 300 
+LIMIT_IMAGE_COUNT = 50 # 1st: 0, 2nd: 36, 3rd: 72 stops, at the first step over 50 
 
 
 # initialize client 
@@ -81,10 +81,11 @@ while (api.feed_tag(HASHTAG, api.generate_uuid())["more_available"] == True) and
         # Extract the value *next_max_id*
         next_max_id = next_page_posts["next_max_id"] 
         all_hash_image_posts_urls.append(next_image_urls)
-        
 
-flat_hash_image_posts_urls = [item for sublist in all_hash_image_posts_urls for item in sublist]
-print(f"A total of {len(flat_hash_image_posts_urls)} image post urls were retrieved from the Instagram page.")
+else:        
+    flat_hash_image_posts_urls = [item for sublist in all_hash_image_posts_urls for item in sublist]
+    print(f"A total of {len(flat_hash_image_posts_urls)} image post urls were retrieved from the Instagram page.")
+    
 
 #%%
 ########### USERNAME ###################
@@ -120,9 +121,9 @@ while (api.username_feed(USERNAME, max_id = next_max_id)["more_available"] == Tr
         next_max_id = next_page_posts["next_max_id"] 
         all_image_posts_urls.append(next_image_urls)
         
-
-flat_image_posts_urls = [item for sublist in all_image_posts_urls for item in sublist]
-print(f"A total of {len(flat_image_posts_urls)} image post urls were retrieved from the Instagram page.")
+else: 
+    flat_image_posts_urls = [item for sublist in all_image_posts_urls for item in sublist]
+    print(f"A total of {len(flat_image_posts_urls)} image post urls were retrieved from the Instagram page.")
 
 
 
@@ -135,16 +136,16 @@ print(f"A total of {len(flat_image_posts_urls)} image post urls were retrieved f
 #%%
 
 #Saving cookies
-cookies = api.cookie_jar.dump()
-with open("cookies.pkl", "wb") as save_cookies:
-    save_cookies.write(cookies)
+# cookies = api.cookie_jar.dump()
+# with open("cookies.pkl", "wb") as save_cookies:
+#     save_cookies.write(cookies)
 
-#Loading cookies
-with open("cookies.pkl", "rb") as read_cookies:
-    cookies = read_cookies.read()
+# #Loading cookies
+# with open("cookies.pkl", "rb") as read_cookies:
+#     cookies = read_cookies.read()
 
-#Pass cookies to Client to resume session
-api = Client(user_name, password, cookie = cookies)
+# #Pass cookies to Client to resume session
+# api = Client(user_name, password, cookie = cookies)
 
 
 
